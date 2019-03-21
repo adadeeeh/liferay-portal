f586803f5d5e360328454c5946ff2353b341092b
Faturrahman <faturrahmanmakruf@gmail.com>
2019-03-21 20:50:23 +0700

    Generating the Back-end

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/21/e0b1afacda4b001914c3f23b4d88de10
@@ -0,0 +1,5 @@
+package com.liferay.docs.guestbook.model;
+
+public class Entry {
+
+}
---
+++ com-liferay-docs-guestbook/modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author pedro
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "guestbook";
+
+}
